e272c25702cd46fec77a05171fb43063e39d9e92
Robinson Lam <robinsonlam94@gmail.com>
2025-09-13 22:33:04 +1000

    reduce fargate resources

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -40,12 +40,12 @@ variable "health_check_grace_period_seconds" {
 
 variable "fargate_cpu" {
   description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = "1024"
+  default     = "256"
 }
 
 variable "fargate_memory" {
   description = "Fargate instance memory to provision (in MiB)"
-  default     = "2048"
+  default     = "512"
 }
 
 variable "mongodb_uri" {
